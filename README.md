1cf9172e9f5530c007948097ef3b93bf95027d4d
Daniel Duclos-Cavalcanti <daniel.duclos.cavalcanti@gmail.com>
2021-12-24 18:25:01 +0100

    # Please include a useful commit message!

--- README.md
+++ README.md
@@ -18,8 +18,12 @@ Hooks reside in `.git/hooks` of every git repository.
 ├── push-to-checkout.sample
 └── update.sample
 ```
-These represent most of the available hooks, but the `.sample` extension prevent them from
+- These represent most of the available hooks, but the `.sample` extension prevent them from
 executing. By removing this ending or adding a script with the same name will enable said
 hook.
 
-There shouldn't be any ending to these files, no `.sh` or `.bash`!!!
+- There shouldn't be any ending to these files, no `.sh` nor `.bash`!!!
+
+- Hooks are mostly local to the repository, however may be applied server-side for
+  example. To be able to have hooks used across a project would mean one would have to
+  either use git `templates` or have the hooks version controlled and symlinked.
---
+++ pre-commit.sh
@@ -0,0 +1,34 @@
+#!/bin/sh
+
+if git rev-parse --verify HEAD >/dev/null 2>&1
+then
+	echo "pre-commit: About to create a new commit!"
+	against=HEAD
+else
+	# Initial commit: diff against an empty tree object
+	echo "pre-commit: About to create the first commit!"
+	against=$(git hash-object -t tree /dev/null)
+fi
+
+# If you want to allow non-ASCII filenames set this variable to true.
+allownonascii=$(git config --type=bool hooks.allownonascii)
+
+# Redirect output to stderr.
+exec 1>&2
+
+if [ "$allownonascii" != "true" ] &&
+	 test $(git diff --cached --name-only --diff-filter=A -z $against |
+	 LC_ALL=C tr -d '[ -~]\0' | wc -c) != 0
+then
+  echo "pre-comit: Attempt to add a non-ASCII file name!"
+	exit 1
+fi
+
+if ! git diff-index --check --cached $against
+then
+  echo "pre-commit: Aborting commit due to whitespace errors!"
+  exit 1
+else
+  echo "pre-commit: No whitespace errors :)"
+  exit 0
+fi
